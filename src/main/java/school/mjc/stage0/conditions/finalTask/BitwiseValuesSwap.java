70bdcf090582221e2258adf4ce01c23f4096ca16
education-my <education.mykhailo@gmail.com>
2022-09-22 03:18:27 +0300

    Module 4 - Final Task

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        second = first ^ second;
+        first = first ^ second;
+        System.out.println(first);
+        second = first ^ second;
+        System.out.println(second);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,14 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import java.time.YearMonth;
+
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if (year < 0 || month < 1 || month > 12) {
+            System.out.println("invalid date");
+        } else {
+            YearMonth yearMonth = YearMonth.of(year, month);
+            System.out.println(yearMonth.lengthOfMonth());
+        }
     }
 }
